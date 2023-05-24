c962ae729093083e339a8b6f15f8190687fca21d
Balázs Benke <benkebalazsggcg@gmail.com>
2023-05-24 20:19:06 +0200

    gömb 1

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ module "read" {
 locals {
   answers = file("modules/write/terraform.tfvars")
 }  
+ 
 module "write" {
   source        = "./modules/write"
   
@@ -19,11 +20,13 @@ module "write" {
   answer_4 = trim(split("=", split("\n", local.answers)[3])[1], "\"\n")
   answer_5 = trim(split("=", split("\n", local.answers)[4])[1], "\"\n")
 }
+  
 module "data" {
    source = "./modules/data"
    file_path = module.files.example_output[0]
    depends_on = [module.files]
-}
+}  
+
 output "file_id" {
    value = module.data.file_id
  }
