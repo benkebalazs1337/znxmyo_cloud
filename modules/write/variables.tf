53b107ba8e2a60250d89dbc6e090bb106dca26f6
Balázs Benke <benkebalazsggcg@gmail.com>
2023-05-24 19:06:50 +0200

    task3

---
+++ modules/write/variables.tf
@@ -0,0 +1,19 @@
+variable "answer_1" {
+  description = "This is the first answer"
+}
+
+variable "answer_2" {
+  description = "This is the second answer"
+}
+
+variable "answer_3" {
+  description = "This is the third answer"
+}
+
+variable "answer_4" {
+  description = "This is the fourth answer"
+}
+
+variable "answer_5" {
+  description = "This is the fifth answer"
+}
